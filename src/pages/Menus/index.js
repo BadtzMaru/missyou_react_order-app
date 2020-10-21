b927a915d770ca4c6a9fc5d92be9ce2f77041483
BadtzMaru <707380414@qq.com>
2020-10-21 23:48:44 +0800

    点餐初步完成

--- src/pages/Menus/index.js
+++ src/pages/Menus/index.js
@@ -13,8 +13,16 @@ class index extends React.Component {
                 key: 'size',
                 title: '尺寸',
                 dataIndex: 'size',
-                render: (text) => {
-                    return <span>{text}</span>;
+                render: (text, record) => {
+                    if (record.price) {
+                        return <span>{text}</span>;
+                    }
+                    return {
+                        children: <strong>{text}</strong>,
+                        props: {
+                            colSpan: 2,
+                        },
+                    };
                 }
             },
             {
@@ -43,7 +51,24 @@ class index extends React.Component {
             },
         ];
         const handleAddMenus = (record) => {
-            console.log(record);
+            let { cart } = this.state;
+            const index = cart.findIndex((item) => item.key === record.key);
+            index >= 0 ? cart.splice(index, 1, {
+                ...cart[index],
+                count: cart[index].count + 1,
+            }) : (cart = [
+                ...cart,
+                {
+                    ...record,
+                    count: 1,
+                }
+            ]);
+            // 存储到状态中
+            this.setState({
+                cart: [
+                    ...cart,
+                ],
+            });
         };
         let data = {
             1: {
@@ -94,15 +119,79 @@ class index extends React.Component {
         );
     }
     renderCartTable() {
-        return (
-            <div>hello</div>
-        );
+        const columns = [
+            {
+                key: 'count',
+                title: '数量',
+                dataIndex: 'count',
+                render: (text, record) => (
+                    <span>
+                        <Button onClick={() => handleDecrease(record)} className={style['cart-btn']}>-</Button>
+                        <span>{record.count}</span>
+                        <Button onClick={() => handleIncrease(record)} className={style['cart-btn']}>+</Button>
+                    </span>
+                ),
+            },
+            {
+                key: 'name',
+                title: '菜单',
+                dataIndex: 'name',
+            },
+            {
+                key: 'price',
+                title: '价格',
+                dataIndex: 'price',
+            },
+        ];
+        const handleDecrease = (record) => {
+            let { cart } = this.state;
+            const index = cart.findIndex((item) => item.key === record.key);
+            const current = cart[index];
+            if (current.count <= 1) {
+                cart.splice(index, 1);
+            } else {
+                cart.splice(index, 1, {
+                    ...current,
+                    count: current.count - 1,
+                });
+            }
+            this.setState({
+                cart,
+            });
+        };
+        const handleIncrease = (record) => {
+            let { cart } = this.state;
+            const index = cart.findIndex((item) => item.key === record.key);
+            const current = cart[index];
+            cart.splice(index, 1, {
+                ...current,
+                count: current.count + 1,
+            });
+            this.setState({
+                cart,
+            });
+        };
+        return <Table
+            rowKey="key"
+            pagination={false}
+            className="menus-table cart"
+            dataSource={this.state.cart}
+            columns={columns}
+            locale={{
+                emptyText: '购物车没有任何商品',
+            }}
+        />;
     }
     render() {
+        const totalPrice = this.state.cart.reduce((total, item) => (total += item.price * item.count), 0);
         return (
             <Row>
                 <Col sm={24} md={16} >{this.renderMenusTable()}</Col>
-                <Col sm={24} md={8} >{this.renderCartTable()}</Col>
+                <Col sm={24} md={8} >
+                    {this.renderCartTable()}
+                    <p className={style['total-price']}>总价: {totalPrice}</p>
+                    <Button className={style['submit-btn']} type="primary">提交</Button>
+                </Col>
             </Row>
         );
     }
